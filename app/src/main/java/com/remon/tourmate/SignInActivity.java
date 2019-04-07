f15e7ba77996521b646b45da95ed9fae8f3dcc9d
hossain72 <hossainanowar72@gmail.com>
2019-04-08 00:20:47 +0600

    Now Complete Sign in page code

--- app/src/main/java/com/remon/tourmate/SignInActivity.java
+++ app/src/main/java/com/remon/tourmate/SignInActivity.java
@@ -1,6 +1,8 @@
 package com.remon.tourmate;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,6 +11,12 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignInActivity extends AppCompatActivity {
 
@@ -17,6 +25,7 @@ public class SignInActivity extends AppCompatActivity {
     private TextInputLayout passwordTIL;
     private Button signInBtn;
     private TextView signUpTV;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +38,8 @@ public class SignInActivity extends AppCompatActivity {
     }
 
     private void initialize() {
+
+        firebaseAuth = FirebaseAuth.getInstance();
 
         logoIV = findViewById(R.id.logoIV);
         emailET = findViewById(R.id.emailET);
@@ -45,6 +56,40 @@ public class SignInActivity extends AppCompatActivity {
             public void onClick(View v) {
                 startActivity(new Intent(SignInActivity.this, MainActivity.class));
             }
+        });
+
+        signInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String email = emailET.getText().toString();
+                String password = passwordTIL.getEditText().getText().toString();
+
+                if (email.equals("") || password.equals("")) {
+                    Toast.makeText(SignInActivity.this, "Please enter required field", Toast.LENGTH_SHORT).show();
+                } else {
+                    loginWithEmailAndPassword(email, password);
+                }
+
+            }
+        });
+
+
+    }
+
+    private void loginWithEmailAndPassword(String email, String password) {
+
+        firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(SignInActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(SignInActivity.this, NavigationActivity.class));
+                } else {
+                    Toast.makeText(SignInActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
+                }
+            }
+
         });
 
     }
